97225eb499bb1e9d2b4a7fb04f4a22952a6599fb
tobi <tobi_mu@yahoo.de>
2021-08-22 22:37:22 +0200

    Added .gitignore and added pyc files to it

--- script.service.pip/resources/lib/m3u.py
+++ script.service.pip/resources/lib/m3u.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 '''
-Copyright (C) 2021  mltobi
+Copyright (C) 2021 mltobi
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
